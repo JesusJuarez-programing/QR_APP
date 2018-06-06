f4a1eb796bd636b1d5fb259eb6f432d8643c92eb
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-06 16:17:23 -0500

    Textos a html

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistorialProvider } from '../../providers/historial/historial';
+import { ScanData } from '../../models/scan-data.model';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  resultado: any;
+  resultado: ScanData[]=[];
 
   constructor(public navCtrl: NavController, private _historialProvider: HistorialProvider) {
-    this.resultado = this._historialProvider.cargar_historial;
-    console.log( this._historialProvider.cargar_historial);
+    
   } 
+
+  ionViewDidLoad(){
+
+    this.resultado = this._historialProvider.cargar_historial();
+    console.log( this._historialProvider.cargar_historial);
+  }
 }
